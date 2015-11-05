808f14c5415e28037ffa96088302786eee934ebc
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-11-05 15:12:00 +0500

    remove express, and add a package.json

--- get_Json_Data.js
+++ get_Json_Data.js
@@ -1,46 +1,39 @@
 var request = require("request");
-module.exports.orderBooks ={
-    post: function (req, res) {
-        var url = "https://api.blinktrade.com/api/v1/PKR/orderbook"
-        request({
-            url: url,
-            json: true
-        }, function (error, response, body) {
+module.exports.orderBooks = function() {
+  var url = "https://api.blinktrade.com/api/v1/PKR/orderbook";
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body) {
 
-            if (!error && response.statusCode === 200) {
-                console.log(body) // Print the json response
-                res.send(body);
-            }
-        })
+    if (!error && response.statusCode === 200) {
+      console.log(body); // Print the json response
     }
-}
-module.exports.ticker ={
-    post: function(req,res){
-      var url = "https://api.blinktrade.com/api/v1/PKR/ticker"
-      request({
-        url: url,
-        json: true
-      }, function (error, response, body) {
+  });
+};
 
-      if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
-        res.send(body);
-       }
-    })
-  }
-}
-module.exports.trades= {
-    post : function(req,res){
-       var url = "https://api.blinktrade.com/api/v1/PKR/trades"
-       request({
-            url: url,
-            json: true
-         }, function (error, response, body) {
+module.exports.ticker = function() {
+  var url = "https://api.blinktrade.com/api/v1/PKR/ticker";
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body) {
 
-          if (!error && response.statusCode === 200) {
-            console.log(body) // Print the json response
-            res.send(body);
-           }
-      })
-   }
-}
+    if (!error && response.statusCode === 200) {
+      console.log(body); // Print the json response
+    }
+  });
+};
+
+module.exports.trades = function() {
+  var url = "https://api.blinktrade.com/api/v1/PKR/trades";
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      console.log(body); // Print the json response
+    }
+  });
+};
--- tester.js
+++ tester.js
@@ -1,16 +1,5 @@
-var orderBook = require('./get_Json_Data.js').orderBooks;
-var ticker = require('./get_Json_Data.js').ticker;
-var trades = require('./get_Json_Data.js').trades;
-var express = require('express');
-var app = express();
+var api = require('./get_Json_Data.js');
 
-app.get('/book', orderBook.post);
-app.get('/ticker', ticker.post);
-app.get('/trades', trades.post);
-
-var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-});
+api.orderBooks();
+api.ticker();
+api.trades();
